4fb7fe835255098c6851dad694808f5a31576f66
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-06 19:01:41 -0300

    Inclement matplotlib pie, render statics number for category

--- expensewebsite/pyfinance/views.py
+++ expensewebsite/pyfinance/views.py
@@ -1,20 +1,50 @@
-from django.shortcuts import render
+import base64
+from io import BytesIO
+from django.shortcuts import get_object_or_404, redirect, render
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 from .forms import ComprasForm
 from .models import Compras
+
+def generate_pie_chart(categories, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=categories, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired.colors)
+    ax.axis('equal') 
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return f'data:image/png;base64,{image_base64}'
+
 def index(request):
-    return render(request, 'index.html')
+    list_category = Compras.objects.all()
+    categories = [item.category for item in list_category]
+    values = [item.value for item in list_category]
+    
+    chart_data = generate_pie_chart(categories, values)
+
+    return render(request, 'index.html',{'chart_data':chart_data})
 
 def shopping(request):
     form = ComprasForm(request.POST)
     if request.method == 'POST':
         category = request.POST.get('category')
         date = request.POST.get('date')
-        value = request.POST.get('value')
-        value = value.replace(",", ".")
-        Compras.objects.create(
-            category=category,
-            date=date,
-            value= value
-        )
+        value = float(request.POST.get('value').replace(",", "."))
 
+        existing_category = get_object_or_404(Compras, category=category)
+
+        if existing_category:
+            existing_category.value += value
+            existing_category.save()
+        else:
+            Compras.objects.create(
+                category=category,
+                date=date,
+                value= value
+            )
     return render(request, "shopping.html", {'form': form})
