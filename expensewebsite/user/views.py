14b63a851dc2744e1f7ee33c96774c5a17f2a628
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-08 19:57:50 -0300

    register user

--- expensewebsite/user/views.py
+++ expensewebsite/user/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.contrib.auth.models import User
 
 def user_finance(request):
     return render(request, 'user_finance.html')
 
 def register_user_finance(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+            )
+        print(user)
+
+        user.save() 
+        return redirect('user_finance')
     return render(request, 'register.html')
